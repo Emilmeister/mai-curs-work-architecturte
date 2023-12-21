57210075b15c5d1ebc87a63d446d41e43fc0c026
Emilmeister <30295628+Emilmeister@users.noreply.github.com>
2023-12-22 03:08:04 +0500

    LR4 cache

--- fast-api-app/app/auth_controller.py
+++ fast-api-app/app/auth_controller.py
@@ -1,21 +1,45 @@
+import json
+
 from fastapi import FastAPI, Response, status
 from models import User
 from repo.user_repo import filter_users, get_user_by_login, create_user, update_user
 import jwt
 import random
 import string
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
+import redis
 
 app = FastAPI()
 jwt_encode_secret = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
+redis_db = redis.Redis(host='redis_host', port=6379, decode_responses=True, username="default", password='pass', encoding='utf8')
+@app.on_event("startup")
+async def start():
+    redis_2 = aioredis.from_url("redis://default:pass@redis_host:6379/0", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis_2), prefix="fastapi-cache")
 
 @app.post("/user/create")
 async def create(user: User):
-    return create_user(user)
+    id = create_user(user)
+    user.id = id
+    redis_db.set("/user/{login}:"+user.login, str(json.dumps(user.model_dump(), indent=4, sort_keys=True, default=str)), 600)
+    return id
+
+
+def key_builder(*args, **kwargs):
+    namespace = args[1]
+    login = kwargs['kwargs']['login']
+    print(namespace+":"+login)
+    return namespace+":"+login
 
 
 @app.get("/user/{login}")
+@cache(expire=600, key_builder=key_builder, namespace="/user/{login}")
 async def get(login):
+    #time.sleep(2)
     result = get_user_by_login(login, 0)
     if result is not None:
         return result
@@ -26,7 +50,10 @@ async def get(login):
 
 @app.post("/user/update")
 async def update(user: User):
-    return update_user(user)
+    id = update_user(user)
+    user.id = id
+    redis_db.set("/user/{login}:" + user.login, str(json.dumps(user.model_dump(), indent=4, sort_keys=True, default=str)), 600)
+    return id
 
 
 @app.get("/user/filter/{surname_mask}/{name_mask}")
